b42f0a996584e6606ce16de9b3e3eb82ae3c7e63
Graham Grochowski <ggrochow@gmail.com>
2025-03-10 12:53:42 -0700

    intial

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <label>
+        Before
+        <input type="file" name="before" />
+      </label>
+
+      <br />
+
+      <label>
+        After
+        <input type="file" name="after" />
+      </label>
+    </main>
+  );
+}
+
+export default App;
